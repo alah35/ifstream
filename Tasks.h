6b0521219f6e3edf6b492ced142364aab30241e8
alah35 <maxnet663@gmail.com>
2022-03-25 13:16:34 +0300

    First commit

---
+++ Tasks.h
@@ -0,0 +1,11 @@
+//
+// Created by maxne on 25.03.2022.
+//
+
+#ifndef IFSTREAM_TASKS_H
+#define IFSTREAM_TASKS_H
+
+#include <iostream>
+#include "Tasks/Task1.cpp"
+
+#endif //IFSTREAM_TASKS_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Tasks.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
